8d91786b0efb0769191b18cd4e368098804dcc17
Tzu-Yang Tseng <50098316+yangtseng@users.noreply.github.com>
2026-07-17 23:09:24 -0500

    Add figures 5E and 5F for gene activity analysis
    Added new figures (5E and 5F) for gene activity analysis with FeatureScatter plots for various cell lines. Calculated total motif-associated fragment count and total accessible motifs count.

--- figure_code/supp_fig5.R
+++ figure_code/supp_fig5.R
@@ -158,3 +158,232 @@ png('supp_fig5b_d.png',
     unit = 'in')
 p
 dev.off()
+
+############################################################################
+### Relationship between ADRN and Gene activity
+
+### Fig. 5E
+png('supp_fig5e_1.png',
+    width = 5,
+    height = 4.5,
+    res = 300,
+    unit = 'in')
+FeatureScatter(object = NB_combined_250617, 
+               feature1 = "nCount_gene_activity",
+               feature2 = 'signature_1PMID286_ADRN_all', 
+               group.by = 'cell_line4', 
+               cells = NB_combined_250617@assays[["RNA"]]@data@Dimnames[[2]][NB_combined_250617$cell_line4 %in% c("SHSY5Y_ctrl") |NB_combined_250617$cell_line4 %in% c("SHSY5Y_treat")],
+               shuffle = T,
+               cols = c('#A8D0C7','#D78D82')) + 
+  xlab("Gene activity nCount") + ylab("ADRN signature gene score") + ggtitle("Cell line: SH-SY5Y (Corr. = 0.19)") + NoLegend() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.title.x = element_text(size = 20, family = 'Arial', vjust = 0),
+        axis.title.y = element_text(size = 20, family = 'Arial'),
+        axis.text.x = element_text(size = 15, family = 'Arial', colour = 'black'),
+        axis.text.y = element_text(size = 15, family = 'Arial', colour = 'black'),
+        axis.line = element_line(size = 1),
+        axis.ticks = element_line(size = 1),
+        legend.text = element_text(size = 15, family = 'Arial'))
+dev.off()
+
+png('supp_fig5e_2.png',
+    width = 5,
+    height = 4.5,
+    res = 300,
+    unit = 'in')
+FeatureScatter(object = NB_combined_250617, 
+               feature1 = "nCount_gene_activity",
+               feature2 = 'signature_1PMID286_ADRN_all', 
+               group.by = 'cell_line4', 
+               cells = NB_combined_250617@assays[["RNA"]]@data@Dimnames[[2]][NB_combined_250617$cell_line4 %in% c("SKNBE2C_ctrl") |NB_combined_250617$cell_line4 %in% c("SKNBE2C_treat")],
+               shuffle = T,
+               cols = c('#A8D0C7','#D78D82')) + 
+  xlab("Gene activity nCount") + ylab("ADRN signature gene score") + ggtitle("Cell line: SK-N-BE(2)C (Corr. = 0.18)") + NoLegend() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.title.x = element_text(size = 20, family = 'Arial', vjust = 0),
+        axis.title.y = element_text(size = 20, family = 'Arial'),
+        axis.text.x = element_text(size = 15, family = 'Arial', colour = 'black'),
+        axis.text.y = element_text(size = 15, family = 'Arial', colour = 'black'),
+        axis.line = element_line(size = 1),
+        axis.ticks = element_line(size = 1),
+        legend.text = element_text(size = 15, family = 'Arial'))
+dev.off()
+
+png('supp_fig5e_3.png',
+    width = 5,
+    height = 4.5,
+    res = 300,
+    unit = 'in')
+FeatureScatter(object = NB_combined_250617, 
+               feature1 = "nCount_gene_activity",
+               feature2 = 'signature_1PMID286_ADRN_all', 
+               group.by = 'cell_line4', 
+               cells = NB_combined_250617@assays[["RNA"]]@data@Dimnames[[2]][NB_combined_250617$cell_line4 %in% c("SKNDZ_ctrl") |NB_combined_250617$cell_line4 %in% c("SKNDZ_treat")],
+               shuffle = T,
+               cols = c('#A8D0C7','#D78D82')) + 
+  xlab("Gene activity nCount") + ylab("ADRN signature gene score") + ggtitle("Cell line: SK-N-DZ (Corr. = 0.19)") + NoLegend() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.title.x = element_text(size = 20, family = 'Arial', vjust = 0),
+        axis.title.y = element_text(size = 20, family = 'Arial'),
+        axis.text.x = element_text(size = 15, family = 'Arial', colour = 'black'),
+        axis.text.y = element_text(size = 15, family = 'Arial', colour = 'black'),
+        axis.line = element_line(size = 1),
+        axis.ticks = element_line(size = 1),
+        legend.text = element_text(size = 15, family = 'Arial'))
+dev.off()
+
+png('supp_fig5e_4.png',
+    width = 5,
+    height = 4.5,
+    res = 300,
+    unit = 'in')
+FeatureScatter(object = NB_combined_250617, 
+               feature1 = "nCount_gene_activity",
+               feature2 = 'signature_1PMID286_ADRN_all', 
+               group.by = 'cell_line4', 
+               cells = NB_combined_250617@assays[["RNA"]]@data@Dimnames[[2]][NB_combined_250617$cell_line4 %in% c("SKNSH_ctrl") |NB_combined_250617$cell_line4 %in% c("SKNSH_treat")],
+               shuffle = T,
+               cols = c('#A8D0C7','#D78D82')) + 
+  xlab("Gene activity nCount") + ylab("ADRN signature gene score") + ggtitle("Cell line: SK-N-SH (Corr. = 0.25)") + NoLegend() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.title.x = element_text(size = 20, family = 'Arial', vjust = 0),
+        axis.title.y = element_text(size = 20, family = 'Arial'),
+        axis.text.x = element_text(size = 15, family = 'Arial', colour = 'black'),
+        axis.text.y = element_text(size = 15, family = 'Arial', colour = 'black'),
+        axis.line = element_line(size = 1),
+        axis.ticks = element_line(size = 1),
+        legend.text = element_text(size = 15, family = 'Arial'))
+dev.off()
+
+### Fig. 5F
+png('supp_fig5f_1.png',
+    width = 5,
+    height = 4.5,
+    res = 300,
+    unit = 'in')
+FeatureScatter(object = NB_combined_250617, 
+               feature1 = "nFeature_gene_activity",
+               feature2 = 'signature_1PMID286_ADRN_all', 
+               group.by = 'cell_line4', 
+               cells = NB_combined_250617@assays[["RNA"]]@data@Dimnames[[2]][NB_combined_250617$cell_line4 %in% c("SHSY5Y_ctrl") |NB_combined_250617$cell_line4 %in% c("SHSY5Y_treat")],
+               shuffle = T,
+               cols = c('#A8D0C7','#D78D82')) + 
+  xlab("Gene activity nFeature") + ylab("ADRN signature gene score") + ggtitle("Cell line: SH-SY5Y (Corr. = 0.18)") + NoLegend() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.title.x = element_text(size = 20, family = 'Arial', vjust = 0),
+        axis.title.y = element_text(size = 20, family = 'Arial'),
+        axis.text.x = element_text(size = 15, family = 'Arial', colour = 'black'),
+        axis.text.y = element_text(size = 15, family = 'Arial', colour = 'black'),
+        axis.line = element_line(size = 1),
+        axis.ticks = element_line(size = 1),
+        legend.text = element_text(size = 15, family = 'Arial'))
+dev.off()
+
+png('supp_fig5f_2.png',
+    width = 5,
+    height = 4.5,
+    res = 300,
+    unit = 'in')
+FeatureScatter(object = NB_combined_250617, 
+               feature1 = "nFeature_gene_activity",
+               feature2 = 'signature_1PMID286_ADRN_all', 
+               group.by = 'cell_line4', 
+               cells = NB_combined_250617@assays[["RNA"]]@data@Dimnames[[2]][NB_combined_250617$cell_line4 %in% c("SKNBE2C_ctrl") |NB_combined_250617$cell_line4 %in% c("SKNBE2C_treat")],
+               shuffle = T,
+               cols = c('#A8D0C7','#D78D82')) + 
+  xlab("Gene activity nFeature") + ylab("ADRN signature gene score") + ggtitle("Cell line: SK-N-BE(2)C (Corr. = 0.16)") + NoLegend() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.title.x = element_text(size = 20, family = 'Arial', vjust = 0),
+        axis.title.y = element_text(size = 20, family = 'Arial'),
+        axis.text.x = element_text(size = 15, family = 'Arial', colour = 'black'),
+        axis.text.y = element_text(size = 15, family = 'Arial', colour = 'black'),
+        axis.line = element_line(size = 1),
+        axis.ticks = element_line(size = 1),
+        legend.text = element_text(size = 15, family = 'Arial'))
+dev.off()
+
+png('supp_fig5f_3.png',
+    width = 5,
+    height = 4.5,
+    res = 300,
+    unit = 'in')
+FeatureScatter(object = NB_combined_250617, 
+               feature1 = "nFeature_gene_activity",
+               feature2 = 'signature_1PMID286_ADRN_all', 
+               group.by = 'cell_line4', 
+               cells = NB_combined_250617@assays[["RNA"]]@data@Dimnames[[2]][NB_combined_250617$cell_line4 %in% c("SKNDZ_ctrl") |NB_combined_250617$cell_line4 %in% c("SKNDZ_treat")],
+               shuffle = T,
+               cols = c('#A8D0C7','#D78D82')) + 
+  xlab("Gene activity nFeature") + ylab("ADRN signature gene score") + ggtitle("Cell line: SK-N-DZ (Corr. = 0.15)") + NoLegend() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.title.x = element_text(size = 20, family = 'Arial', vjust = 0),
+        axis.title.y = element_text(size = 20, family = 'Arial'),
+        axis.text.x = element_text(size = 15, family = 'Arial', colour = 'black'),
+        axis.text.y = element_text(size = 15, family = 'Arial', colour = 'black'),
+        axis.line = element_line(size = 1),
+        axis.ticks = element_line(size = 1),
+        legend.text = element_text(size = 15, family = 'Arial'))
+dev.off()
+
+png('supp_fig5f_4.png',
+    width = 5,
+    height = 4.5,
+    res = 300,
+    unit = 'in')
+FeatureScatter(object = NB_combined_250617, 
+               feature1 = "nFeature_gene_activity",
+               feature2 = 'signature_1PMID286_ADRN_all', 
+               group.by = 'cell_line4', 
+               cells = NB_combined_250617@assays[["RNA"]]@data@Dimnames[[2]][NB_combined_250617$cell_line4 %in% c("SKNSH_ctrl") |NB_combined_250617$cell_line4 %in% c("SKNSH_treat")],
+               shuffle = T,
+               cols = c('#A8D0C7','#D78D82')) + 
+  xlab("Gene activity nFeature") + ylab("ADRN signature gene score") + ggtitle("Cell line: SK-N-SH (Corr. = 0.22)") + NoLegend() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.title.x = element_text(size = 20, family = 'Arial', vjust = 0),
+        axis.title.y = element_text(size = 20, family = 'Arial'),
+        axis.text.x = element_text(size = 15, family = 'Arial', colour = 'black'),
+        axis.text.y = element_text(size = 15, family = 'Arial', colour = 'black'),
+        axis.line = element_line(size = 1),
+        axis.ticks = element_line(size = 1),
+        legend.text = element_text(size = 15, family = 'Arial'))
+dev.off()
+
+
+#############################################################
+
+# Calculate Total Motif-Associated Fragment Count (nCount_chromvar)
+NB_combined_250617$cell_line_prefix <- as.factor(as.character(str_extract(NB_combined_250617@meta.data[["cell_line4"]], "[^_]+")))
+sesoi_table <- NB_combined_250617@meta.data %>%
+  group_by(cell_line_prefix) %>%
+  summarise(
+    mean_count = mean(nCount_gene_activity, na.rm = TRUE),
+    sd_count   = sd(nCount_gene_activity, na.rm = TRUE),
+    sample_size = n()
+  ) %>%
+  mutate(SESOI = 0.5 * sd_count)
+
+# 2. Calculate the Global Pooled SD (Recommended for general benchmarking)
+# This calculates the square root of the average within-group variance
+pooled_sd <- sqrt(sum((sesoi_table$sample_size - 1) * sesoi_table$sd_count^2) / 
+                    sum(sesoi_table$sample_size - 1))
+
+global_sesoi <- 0.5 * pooled_sd
+# Calculate Total Accessible Motifs Count (nFeature_chromvar)
+# The 'counts' slot is where the raw counts are stored. Counting features with >0 fragments.
+NB_combined$nFeature_chromvar <- Matrix::colSums(NB_combined@assays[["chromvar"]]$counts > 0)
+sesoi_table <- NB_combined_250617@meta.data %>%
+  group_by(cell_line_prefix) %>%
+  summarise(
+    mean_count = mean(nFeature_gene_activity, na.rm = TRUE),
+    sd_count   = sd(nFeature_gene_activity, na.rm = TRUE),
+    sample_size = n()
+  ) %>%
+  mutate(SESOI = 0.5 * sd_count)
